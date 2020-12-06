309aa9f89f205b1e5fea7442a613ff4ec72f6a80
Andreina Parra <andreinamariap@gmail.com>
2020-12-06 20:11:41 -0300

    actualizacion

--- contact.php
+++ contact.php
@@ -1,30 +1,45 @@
 <?php
-$nombre = $_POST['nombre'];
-$email = $_POST['email'];
-$mensaje = $_POST['mensaje'];
-$bandeja = $_POST['bandeja'];
-$pago = $_POST['pago'];
-$calle = $_POST['calle'];
-$altura = $_POST['altura'];
-$altura = $_POST['zona'];
-$mensaje = $_POST['mensaje'];
-$bandeja = $_POST['bandeja'];
-$pago = $_POST['pago']
+if(isset($_POST['email'])) {
 
-$mensaje = $_POST['mensaje'];
-$para = 'TUCORREO@CORREO.COM';
-$titulo = 'ASUNTO DEL MENSAJE';
-$header = 'From: ' . $email;
-$msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Mensaje:\n $mensaje";
-  
-if ($_POST['submit']) {
-if (mail($para, $titulo, $msjCorreo, $header)) {
-echo "<script language='javascript'>
-alert('Mensaje enviado, muchas gracias.');
-window.location.href = 'http://TUSITIOWEB.COM';
-</script>";
-} else {
-echo 'Falló el envio';
+<bold>// Debes editar las próximas dos líneas de código de acuerdo con tus preferencias</bold>
+$email_to = "andreinamariap@gmail.com";
+$email_subject = "Pedidos Rolls";
+
+<bold>// Aquí se deberían validar los datos ingresados por el usuario</bold>
+if
+(!isset($_POST['nombre']) ||
+!isset($_POST['email']) ||
+!isset($_POST['telefono']) ||
+!isset($_POST['bandeja'])) 
+(!isset($_POST['pago']) ||
+!isset($_POST['calle']) ||
+!isset($_POST['altura']) ||
+!isset($_POST['zona']) ||
+!isset($_POST['mensaje'])) {
+
+echo "<b>Ocurrió un error y el formulario no ha sido enviado. </b><br />";
+echo "Por favor, vuelva atrás y verifique la información ingresada<br />";
+die();
 }
+
+$email_message = "Detalles del formulario de contacto:\n\n";
+$email_message .= "Nombre: " . $_POST['nombre'] . "\n";
+$email_message .= "Email: " . $_POST['email'] . "\n";
+$email_message .= "Telefono: " . $_POST['telefono'] . "\n";
+$email_message .= "Bandeja: " . $_POST['bandeja'] . "\n";
+$email_message .= "Pago: " . $_POST['pago'] . "\n";
+$email_message .= "Calle: " . $_POST['calle'] . "\n";
+$email_message .= " altura: " . $_POST['altura'] . "\n";
+$email_message .= "Zona: " . $_POST['zona'] . "\n";
+$email_message .= "Comentarios: " . $_POST['mensaj'] . "\n\n";
+
+
+<bold>// Ahora se envía el e-mail usando la función mail() de PHP</bold>
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);
+
+echo "¡El formulario se ha enviado con éxito!";
 }
 ?>
