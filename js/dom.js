c22db67327b43859cf02758dd71c6385d02e17fa
Andreina Parra <andreinamariap@gmail.com>
2021-01-30 16:52:09 -0300

    segunda entrega

--- js/dom.js
+++ js/dom.js
@@ -1,28 +1,32 @@
 class DOMcarrito {
     constructor() {
-
-    }
-    h2(nombre) {
-        const h2 = document.createElement('h2');
-        h2.textContent = nombre;
-        return h2;
-    }
-
-    p(contenido) {
-        const p = document.createElement('p');
-        p.textContent = contenido;
-
-        return p;
     }
 
     img (src) {
 
         const img = document.createElement ('img');
+        img.classList.add ('img3');
         img.src = src;
 
         return img;
     }
 
+    h2(nombre) {
+        const h2 = document.createElement('h2');
+        h2.textContent = nombre;
+        h2.classList.add ('p2');
+        return h2;
+    }
+
+    p(contenido) {
+        const p = document.createElement('p');
+        p.classList.add ('p3');
+        p.textContent = contenido;
+
+        return p;
+    }
+
+  
     button(content, className, id) {
         const button = document.createElement('button');
         button.textContent = content;
--- js/json.js
+++ js/json.js
@@ -1,25 +1,28 @@
 const bandeja = [{
     'id': 0,
+    'imagen': 'img/img1.jpg',
     'nombre': 'bandeja 1',
     'cantidad': '4 rolls',
     'precio': '$100',
-    'imagen': '',
+    
 }, {
     'id': 1,
+    'imagen': 'img/img2.jpg',
     'nombre': 'bandeja 2',
     'cantidad': '6 rolls',
     'precio': '$200',
-    'imagen': '',
+   
 }, {
     'id': 2,
+    'imagen': 'img/img3.jpg',
     'nombre': 'bandeja 3',
     'cantidad': '8 rolls',
     'precio': '$300',
-    'imagen': '',
+    
 }, {
     'id': 3,
+    'imagen': 'img/img4.jpg',
     'nombre': 'bandeja 4',
     'cantidad': '12 rolls',
-    'precio': '$400',
-    'imagen': '',
+    'precio': '$400',  
 }]
